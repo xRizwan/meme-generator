32d307f73b65d9ecb8e3e78568a7f9a35ce9e903
xrizwan <xrizwanr@gmail.com>
2020-08-06 19:05:46 -0700

    Updates

---
+++ precache-manifest.357657cc97b63af367b8bf542ec8dd8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84be641c22772c20957ce13cb9880fb",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "213f0b98f633fa189c73",
+    "url": "/meme-generator/static/css/main.50a82bf2.chunk.css"
+  },
+  {
+    "revision": "29ea514b03c1dfc7a337",
+    "url": "/meme-generator/static/js/2.809d901d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.809d901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213f0b98f633fa189c73",
+    "url": "/meme-generator/static/js/main.b7852c47.chunk.js"
+  },
+  {
+    "revision": "4c096d4f2ab9a1b6351f",
+    "url": "/meme-generator/static/js/runtime-main.bba219dc.js"
+  }
+]);
